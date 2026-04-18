d06dd933e9a19eb181f7451168f332a3b567df9a
shishirbasel482-boop <shishirbasel482@gmail.com>
2026-04-18 09:35:41 +0800

    practice

---
+++ practice.py
@@ -0,0 +1,13 @@
+a = int(input("Enterv 1st number:"))
+b = int(input("Enter 2nd number:"))
+
+sum = a + b
+print(sum)
+
+side = int(input("Enter the length of square:"))
+print("Area of square is:", side ** 2)
+
+a = float(input("Enter 1st value:"))
+b = float(input("Enter 2nd value:"))
+avg = (a + b)/2
+print("The avg of two floating value is:", avg)
