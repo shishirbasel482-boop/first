b6b73285a5e7e6d826539332f867b59a485f28d8
shishirbasel482-boop <shishirbasel482@gmail.com>
2026-04-21 11:46:10 +0800

    Day 1 of practcing

---
+++ practice2.py
@@ -0,0 +1,17 @@
+"""
+# WAP to input user frist name and find its length.
+name = input("Enter your name:")
+length = len(name)
+print(length)
+
+# WAP to find the occurance of a character in a string.
+string = "My name is a Shishir a Baasel."
+occurance = string.count("a")
+print(occurance)
+"""
+# WAP to check number is even or odd.
+num = int(input("Enter a number"))
+if (num % 2 == 0):
+    print(The number is even.)
+else
+    print(The number is odd.)
