bcd8e48e28be0dd3a280ec9c749ecf63d2040fc7
shishirbasel482-boop <shishirbasel482@gmail.com>
2026-04-28 11:36:10 +0800

    It contains week 2 all codes

--- practice2.py
+++ practice2.py
@@ -8,10 +8,21 @@ print(length)
 string = "My name is a Shishir a Baasel."
 occurance = string.count("a")
 print(occurance)
-"""
+
 # WAP to check number is even or odd.
 num = int(input("Enter a number"))
 if (num % 2 == 0):
     print(The number is even.)
 else
     print(The number is odd.)
+
+Int_list = [5, 10, 20, 25, 30]
+print("Original list:", Int_list)
+del my_list[1] # Deletes the element at index 1 (10)
+print("After del:", Int_list) # Output: [5, 20, 25, 30]g
+"""
+message = input("Enter a message or quit to exit: ")
+
+while message != 'quit':
+    print(message)
+    message = input("Enter another message or quit to exit: ")
---
+++ week2.py
@@ -0,0 +1,6 @@
+# Syntax is clean and readable.
+x = 5
+if x < 10:
+    print("x is less than 10")
+else:
+     print("x is 10 or more")
